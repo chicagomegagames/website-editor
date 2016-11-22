c72a456119ef7a26d4e40db161f4858a532c2a5c
Don Kuntz <don@kuntz.co>
2016-11-21 19:57:25 -0600

    Cleanup static.publish_site
    It will now return errors when they exist. Also the logic was cleaned up
    to not include duplicate code.

--- app/static.py
+++ app/static.py
@@ -4,8 +4,9 @@ import shutil
 from .models import Game, Page, Event
 
 def publish_site(location=os.path.join(os.getcwd(), "_site"), theme="default"):
+    errors = []
+
     if os.path.exists(location):
-        #shutil.rmtree(location)
         root, dirs, files = next(os.walk(location))
         for d in dirs:
             shutil.rmtree(os.path.join(root, d))
@@ -24,7 +25,16 @@ def publish_site(location=os.path.join(os.getcwd(), "_site"), theme="default"):
     calendar = Event.future_events()
     site = {"games": games, "pages": pages, "calendar": calendar}
 
-    for page in pages:
+    pages_to_generate = list(pages)
+    for game in games:
+        slug = game.metadata["slug"]
+        if slug[0] == "/":
+            slug = slug[1:]
+
+        game.metadata["slug"] = os.path.join("games", slug)
+        pages_to_generate.append(game)
+
+    for page in pages_to_generate:
         slug = page.metadata["slug"]
         if slug[0] == "/":
             slug = slug[1:]
@@ -33,36 +43,25 @@ def publish_site(location=os.path.join(os.getcwd(), "_site"), theme="default"):
         output_file = os.path.join(output_directory, "index.html")
         make_directory_tree(output_directory)
 
-        if "layout" in page.metadata:
+        if "layout" in page.metadata and page.metadata["layout"]:
             layout = page.metadata["layout"]
         else:
             layout = "page.html"
 
-        template = templ_env.get_template(layout)
-        with open(output_file, "w+") as writer:
-            writer.write(template.render(site=site, page=page))
-
-    for game in games:
-        slug = game.metadata["slug"]
-        if slug[0] == "/":
-            slug = slug[1:]
-
-        output_directory = os.path.join(location, "games", slug)
-        output_file = os.path.join(output_directory, "index.html")
-        make_directory_tree(output_directory)
-
-        if "layout" in game.metadata:
-            layout = game.metadata["layout"]
-        else:
-            layout = "page.html"
-
-        template = templ_env.get_template(layout)
-        with open(output_file, "w+") as writer:
-            writer.write(template.render(site=site, page=game))
+        try:
+            template = templ_env.get_template(layout)
+            with open(output_file, "w+") as writer:
+                writer.write(template.render(site=site, page=page))
+        except jinja2.exceptions.TemplateNotFound as e:
+            error = "Template not found `{}` for `{}`".format(layout, slug)
+            errors.append(error)
+            return error
 
     assets_dir = os.path.join(theme_dir, "assets")
     if os.path.exists(assets_dir):
         shutil.copytree(assets_dir, os.path.join(location, "assets"))
+
+    return None
 
 
 def make_directory_tree(path):
