0a412018bf348db95297342806aeaf81953245d4
Don Kuntz <don@kuntz.co>
2016-12-14 10:54:57 -0800

    Make deploy task a lil better

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 from fabric.api import task, env, sudo, run
 from fabric.operations import prompt
-from fabric.context_managers import cd
+from fabric.context_managers import cd, hide
 from datetime import datetime
 import subprocess
 
@@ -24,8 +24,10 @@ def get_revision():
 
     tags = out.decode("utf8", "strict").strip().split("\n")
     tags.reverse()
+    recent_tags = tags[0:5]
+    recent_tags.reverse()
     print("Recent git tags:")
-    for tag in tags[0:5]:
+    for tag in recent_tags:
         print("    {}".format(tag))
 
     revision = prompt("Git revision to deploy:")
@@ -40,24 +42,25 @@ def get_revision():
 
 @task
 def deploy():
-    deploy_time = datetime.now().strftime("%Y%m%d%H%M%S")
-    revision = get_revision()
+    with(hide('stdout')):
+        deploy_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        revision = get_revision()
 
-    sudo("chown {user}:{user} {path}".format(user=env.user, path=git_checkout_path ))
-    with cd(git_checkout_path):
-        run("git fetch --tags {}".format(git_repo))
-        run("git checkout {}".format(revision))
+        sudo("chown {user}:{user} {path}".format(user=env.user, path=git_checkout_path ))
+        with cd(git_checkout_path):
+            run("git fetch --tags {}".format(git_repo))
+            run("git checkout {}".format(revision))
 
 
-    revision_directory = "{}/revisions/{}".format(deploy_location, deploy_time)
-    sudo("mkdir {}".format(revision_directory), user="manager")
-    sudo("rsync -ac {}/ {}".format(git_checkout_path, revision_directory), user="manager")
+        revision_directory = "{}/revisions/{}".format(deploy_location, deploy_time)
+        sudo("mkdir {}".format(revision_directory), user="manager")
+        sudo("rsync -ac {}/ {}".format(git_checkout_path, revision_directory), user="manager")
 
-    current_directory = "{}/current".format(deploy_location)
-    sudo("ln -sfT {} {}".format(revision_directory, current_directory), user="manager")
-    sudo("service megagame_manager restart")
+        current_directory = "{}/current".format(deploy_location)
+        sudo("ln -sfT {} {}".format(revision_directory, current_directory), user="manager")
+        sudo("service megagame_manager restart")
 
-    output = sudo("ls {}/revisions".format(deploy_location)).strip().split("\n")
-    output.reverse()
-    for directory in output[5:]:
-        sudo("rm -rf {}/revisions/{}".format(deploy_location, directory), user="manager")
+        output = sudo("ls {}/revisions".format(deploy_location)).strip().split("\n")
+        output.reverse()
+        for directory in output[5:]:
+            sudo("rm -rf {}/revisions/{}".format(deploy_location, directory), user="manager")
