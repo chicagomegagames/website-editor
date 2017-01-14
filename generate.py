258bbde8f2cf27a26b2112429159b90936a64622
Don Kuntz <don@kuntz.co>
2017-01-14 12:52:48 -0600

    Add script to easily generate the site on the fly, without booting up the editor application itself

---
+++ generate.py
@@ -0,0 +1,22 @@
+from app import GeneratorService, ImageService
+from app.models import BaseModel
+import argparse
+import os.path
+
+def main():
+    parser = argparse.ArgumentParser(description="Just generate the site.")
+    parser.add_argument("content", type=str, help="Where the content is stored")
+    parser.add_argument("location", type=str, help="Where to generate the site")
+    parser.add_argument("--theme", type=str, help="Theme to generate", required=True)
+
+    args = parser.parse_args()
+
+    BaseModel.set_base_dir(args.content)
+    image_service = ImageService(os.path.join(args.content, "images"))
+    theme_path = os.path.join(args.content, "themes", args.theme)
+    generator = GeneratorService(None, None, default_theme_path = theme_path)
+
+    generator.generate(args.location, theme_path = theme_path, image_service = image_service)
+
+if __name__ == "__main__":
+    main()
