c7a140006c334e338e20b8fd595614c6a4876a9d
Don Kuntz <don@kuntz.co>
2017-04-11 17:44:30 -0500

    deployment: Better git tag sorting for tag prompt
    The old sorting would sort:
        v1.1
        v1.10
        v1.11
        v1.2
        ....
        v1.9
    
    This is non-optimal, as it would mean of those only v1.9 would be shown.
    
    The new sorting is:
        v1.1
        v1.2
        ...
        v1.9
        v1.10
        v1.11
    
    Future work would be pulling out the git-related things into a separate
    library or something similar.

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,9 @@
 from fabric.api import task, env, sudo, run
 from fabric.operations import prompt
 from fabric.context_managers import cd, hide
+from functools import cmp_to_key
 from datetime import datetime
+import re
 import subprocess
 
 env.hosts = [
@@ -13,7 +15,7 @@ git_checkout_path = "/tmp/git_repos/website-editor"
 git_repo = "git@gitlab.com:megagames/website-editor.git"
 deploy_location = "/home/manager/megagame_manager"
 
-def get_revision():
+def recent_git_tags():
     git_cmd = subprocess.Popen(["git", "tag"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = git_cmd.communicate()
 
@@ -23,11 +25,44 @@ def get_revision():
         raise Exception("Errors in local command `{}`\n{}".format(command, err.decode("utf8", "strict")))
 
     tags = out.decode("utf8", "strict").strip().split("\n")
-    tags.reverse()
-    recent_tags = tags[0:5]
+
+    regex = re.compile(r"^v(\d+).(\d+)(|.)(\d*)$", re.M)
+    def int_convert(string):
+        try:
+            return int(string)
+        except ValueError:
+            return 0
+
+    def tag_sort(tag1, tag2):
+        r1 = regex.match(tag1)
+        r2 = regex.match(tag2)
+
+        if r1 and not r2:
+            return 1
+        if r2 and not r2:
+            return -1
+
+        v1 = list(map(int_convert, [r1.group(1), r1.group(2), r1.group(4)]))
+        v2 = list(map(int_convert, [r2.group(1), r2.group(2), r2.group(4)]))
+
+        if v1[0] != v2[0]:
+            return v1[0] - v2[0]
+        if v1[1] != v2[1]:
+            return v1[1] - v2[1]
+        if v1[2] != v2[2]:
+            return v1[2] - v2[2]
+
+        return tag1 - tag2
+
+    sorted_tags = sorted(tags, key=cmp_to_key(tag_sort))
+    sorted_tags.reverse()
+    recent_tags = sorted_tags[0:5]
     recent_tags.reverse()
+    return recent_tags
+
+def get_revision():
     print("Recent git tags:")
-    for tag in recent_tags:
+    for tag in recent_git_tags():
         print("    {}".format(tag))
 
     revision = prompt("Git revision to deploy:")
