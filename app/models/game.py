849ed120a8f238ace03dcd62c5670f1d456d12e8
Don Kuntz <don@kuntz.co>
2017-04-11 17:33:53 -0500

    models and editing: Add metadata hinting to games (and edit page)
    This allows end users to more easily determine what exactly a metadata
    field is for, because there are starting to be many, and their names
    aren't the best.

--- app/models/game.py
+++ app/models/game.py
@@ -5,19 +5,45 @@ class Game(BaseModel):
     CONTENT_DIR = "games"
     ROUTE_PREFIX = "game"
     REQUIRED_META = {
-        "name": {"type": "text"},
+        "name": {
+            "type": "text",
+            "hint": "The name of the game",
+        },
     }
 
     OPTIONAL_META = {
-        "control_count": {"type": "text"},
-        "credits": {"type": "markdown"},
-        "game_length": {"type": "text"},
-        "hero_alt": {"type": "text"},
-        "hero_caption": {"type": "text"},
-        "hero_image": {"type": "image"},
-        "layout": {"type": "text"},
-        "player_count": {"type": "text"},
-        "preview_image": {"type": "image"},
+        "control_count": {
+            "type": "text",
+            "hint": "The number of Control this game requires (ex '5-8')",
+        },
+        "credits": {
+            "type": "markdown",
+            "hint": "(markdown) Who built the game, and what they contributed",
+        },
+        "game_length": {
+            "type": "text",
+            "hint": "How long this game typically lasts (ex '5 hours')",
+        },
+        "hero_image": {
+            "type": "image",
+            "hint": "The big image that goes behind the name and subtitle",
+        },
+        "layout": {
+            "type": "text",
+            "hint": "filename of the template this page should be rendered with (ex 'page.html', 'game.html')",
+        },
+        "player_count": {
+            "type": "text",
+            "hint": "The number of players this game supports (ex '40-50')",
+        },
+        "preview_image": {
+            "type": "image",
+            "hint": "The image used on the All Games page for this game",
+        },
+        "subtitle": {
+            "type": "text",
+            "hint": "Subtitle for the game (ex 'the Chicago-style zombie survival megagame')",
+        }
     }
 
     @classmethod
