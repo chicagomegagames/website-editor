33130c7d56245dce1dad68a2d4a7497ab51d8a2c
Don Kuntz <don@kuntz.co>
2018-01-14 18:12:58 -0600

    Update EventControllerTest to use database

--- tests/controllers/event_controller_test.py
+++ tests/controllers/event_controller_test.py
@@ -9,7 +9,8 @@ class EventControllerTest(ControllerTest):
         expect(response).to(have_in_body("No events created"))
 
     def test_index_with_models(self):
-        event = Event.create(filename="january_wts.md", name="Watch the Skies", date="January 28, 2017", location="Location TBD")
+        event = factory(Event).create(name = "Watch the Skies", date = "January 28, 2017", location = "Location TBD")
+        event.save()
 
         response = self.app.get("/events/")
         expect(response).to(be_successful)
@@ -25,9 +26,9 @@ class EventControllerTest(ControllerTest):
 
     def test_post_new(self):
         response = self.app.post("/events/new", data = {
-            "metadata[name]": "Don's Cool Event",
-            "metadata[date]": "December 20, 2016",
-            "metadata[location]": "TBD",
+            "name": "Don's Cool Event",
+            "date": "December 20, 2016",
+            "location": "TBD",
             "markdown": "This is going to be one cool event guys.",
         })
 
